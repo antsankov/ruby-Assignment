88df5ec23be4d5e9c9f8ef7761a524b48831451a
saasbook <saasbook@saasbook.(none)>
2014-01-31 18:11:49 -0400

    Finished it all up

--- ruby_project/part_8.rb
+++ ruby_project/part_8.rb
@@ -1,8 +1,11 @@
-class Class # worked off of github http://stackoverflow.com/questions/11604149/initializing-attr-accessor-like-method-with-history
+#REMEBER FOR INTERVIEW: YOU CAN OPEN ANY METHOD OR CLASS DURING RUN TIME 
+
+class Class # worked off of stack http://stackoverflow.com/questions/11604149/initializing-attr-accessor-like-method-with-history
   def attr_accessor_with_history(attr_name) #creates the accessor 
     attr_name = attr_name.to_s #turns it into a string
     attr_reader attr_name #sets up a reader
-    class_eval %Q{
+    #what this basically does is it inputs the following code as literal ascii characters into the Class starting from the eval function
+    class_eval %Q{ 
       def #{attr_name}_history
         @#{attr_name}_history || [nil] # give default value if not assigned
       end
@@ -15,13 +18,15 @@ class Class # worked off of github http://stackoverflow.com/questions/11604149/i
   end
 end
 
-class Example
-  attr_accessor_with_history :foo
+class Tester
+  attr_accessor_with_history :foo #sets up the acesseor with history
   attr_accessor_with_history :bar
 end
 
-a = Example.new; a.foo = 2; a.foo = "test"; 
-p a.foo_history # => [nil, 2, "test"]
+a = Tester.new
+a.foo = 100
+a.foo = "test"
+p a.foo_history # => returns nil, 2, "test"
 
-a = Example.new
-p a.foo_history # => [nil]
+a = Tester.new
+p a.foo_history # => just returns nil 
--- ruby_project/part_9.rb
+++ ruby_project/part_9.rb
@@ -1,24 +1,23 @@
 class Numeric
 
- @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+ @@currencies = {'dollar' => 1.0, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019} #sets up our aray of currencies
 
- def method_missing(method_id)
-
-   singular_currency = method_id.to_s.gsub( /s$/,'')
-
-   if @@currencies.has_key?(singular_currency)
-
-     self * @@currencies[singular_currency]
-
+ def method_missing(method_id) #search for method, if it doesn't have it go up the chain, doesn't find it, and comes back and finds method missing
+   singular_currency = method_id.to_s.gsub( /s$/, '') #if there is an 's' at the end take it out 
+   if @@currencies.has_key?(singular_currency) #check if we have the currency 
+     self * @@currencies[singular_currency] # This multiplys the self by the multiplier, creates a method on the fly
    else
-
-     super
-
+     super #just return the super
    end
-
  end
 
+def in(currency)
+	singular_currency = currency.to_s.gsub( /s$/, '')
+	self / @@currencies[singular_currency] #this reverses the program 
 end
 
-5.dollars.in(:euros)
-10.euros.in(:rupees)
+end
+# if you add a new mo
+p 5.euros.in(:dollars)                
+p 0.25.dollars.in(:euro)              
+p 1.dollar.in(:euros).in(:dollar)    
