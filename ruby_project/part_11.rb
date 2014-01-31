a252506c804e3d0862ebb2df0c88f561af91c995
saasbook <saasbook@saasbook.(none)>
2014-01-31 00:14:20 -0400

    Comment 8, and finish 9

--- ruby_project/part_11.rb
+++ ruby_project/part_11.rb
@@ -2,20 +2,21 @@ class CartesianProduct
 
 	attr_accessor:set1
 	attr_accessor:set2
+	attr_accessor:final_set
 
-	def initialize(set1,set2)
+	def initialize(set1,set2,final_set)
 		@set1 = set1
 		@set2 = set2
-
+		@final_set = final_set
 	end 
-
+	final_set= []
 	def cart_prob()
-
-		 self.set1.each { |x| self.set2.each{|y| p y*x}}
-
+		
+		self.set1.each { |x| self.set2.each{|y|  final_set << [x,y]}}
+		print final_set
 	end
 end
 
-test = CartesianProduct.new([1,2,3,10],[4,2,1])
+test = CartesianProduct.new([:b,:d,:b,3],[4,2,1],[])
 
 test.cart_prob()
--- ruby_project/part_8.rb
+++ ruby_project/part_8.rb
@@ -1,35 +1,27 @@
-class Class
+class Class # worked off of github http://stackoverflow.com/questions/11604149/initializing-attr-accessor-like-method-with-history
+  def attr_accessor_with_history(attr_name) #creates the accessor 
+    attr_name = attr_name.to_s #turns it into a string
+    attr_reader attr_name #sets up a reader
+    class_eval %Q{
+      def #{attr_name}_history
+        @#{attr_name}_history || [nil] # give default value if not assigned
+      end
 
-    def attr_accessor_with_history(attr_name)
-
-        attr_name = attr_name.to_s       # make sure it's a string
-
-        attr_reader attr_name            # create the attribute's getter
-
-        attr_reader attr_name+"_history" # create bar_history getter
-
-        #class_eval "your code here, use %Q for multiline strings"
-
-    end
-
+      def #{attr_name}=(new_value)
+        @#{attr_name}_history ||= [nil] # shortcut, compare to your line
+        @#{attr_name}_history << @#{attr_name} = new_value
+      end
+    }
+  end
 end
 
- 
-
-class Foo
-
-    attr_accessor_with_history :bar
-
+class Example
+  attr_accessor_with_history :foo
+  attr_accessor_with_history :bar
 end
- 
-f = Foo.new
 
-f.bar = 1
+a = Example.new; a.foo = 2; a.foo = "test"; 
+p a.foo_history # => [nil, 2, "test"]
 
-p f.bar
-
-f.bar = 10
-
-p f.bar
-
-p f.bar_history
+a = Example.new
+p a.foo_history # => [nil]
